a963f6ff858c62c51805d8bdb8df12129130f97d
jihee2656 <jihee_choi12@naver.com>
2023-01-10 09:35:44 +0900

    feat: Add container(trailing comma test)
    before

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
 # Set username
 username = "John Doe"
 # Print greeting with username
-print('Hello,', username)
+print('Hello,', username)    
+        
+print('Hello', username)
+
+#Trailing comma
+menus = [
+    'Ramen',
+    'Bear soup',
+]
---
+++ numguess.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+# Generate answer
+answer = randint(1,100)
+
+# Print answer for debugging
+print(answer)
+
+#user_name
+username= input("Hi, What is your name?")
+print(f"hi, {username}! Please be my guesr!!")
+guess = int(input("so {}, Guess the number(1-100): ")
+print(f'well choice {}`~ you picked{}!!')
